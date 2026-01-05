d66b09850e88433c6576888b780c9795262dbaca
lascano26 <laurenlascano26@gmail.com>
2026-01-04 19:04:59 -0500

    fix: unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Homepage from '../pages/Homepage'
 import './App.css'
 
